1bd97216381a336c201aa52627fdf29f9c4ffd52
pondelion <test@test.com>
2018-10-21 13:29:02 +0900

    パラメータGUI周り更新

--- frontend/js/api_call.js
+++ frontend/js/api_call.js
@@ -1,17 +1,17 @@
 
-function get_request(url, callback) {
+function getRequest(url, callback) {
     var request = new XMLHttpRequest();
-    request.open("get", endpoint);
+    request.open("get", url);
     request.onreadystatechange = callback;
     request.send();
 }
 
-function get_simulators(ip, port, callback) {
+function getSimulators(ip, port, callback) {
     var endpoint = "http://" + ip + ":" + port + "/get_simulators?simulator=bouncing_ball";
-    get_request(endpoint, callback);
+    getRequest(endpoint, callback);
 }
 
-function get_param_names(ip, port, callback, simulator) {
+function getParamNames(ip, port, callback, simulator) {
     var endpoint = "http://" + ip + ":" + port + "/get_param_names?simulator=" + simulator;
-    get_request(endpoint, callback);
+    getRequest(endpoint, callback);
 }
