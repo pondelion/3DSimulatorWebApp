47f576f35d262e9d60f57fa524100f8749ae5f95
pondelion <programming.deve@gmail.com>
2018-12-15 19:21:57 +0900

    add c library

---
+++ backend/clib/src/simulators/condensed_matter/spin/ising_spin_2D.cpp
@@ -0,0 +1,49 @@
+#include "ising_spin_2D.hpp"
+
+
+IsingSpin2D::IsingSpin2D(int dimX, int dimY, double initTemperature) : 
+    mDimX(dimX),
+    mDimY(dimY),
+    mTemperature(initTemperature),
+    mSpins(MatrixXd::Zero(dimX, dimY))
+{
+};
+
+void IsingSpin2D::update(double dt)
+{
+    using namespace std;
+    //random_device rd
+    chrono::system_clock::time_point start;
+    double elapsed;
+    start = chrono::system_clock::now();
+    while (elapsed < dt)
+    {
+        mCnt++;
+        for (auto i = 0; i < mDimX; ++i) {
+            for (auto j = 0; j < mDimY; ++j) {
+                //mArr[i][j] = mCnt;
+                if (0.5 < (rd()+1)/RAND_MAX) {
+                    mSpins(i, j) = 1;
+                } else {
+                    mSpins(i, j) = 0;
+                }
+            }
+        }
+        elapsed = static_cast<double>(chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() / 1000.0);
+    }
+};
+
+
+np::ndarray IsingSpin2D::getSpins()
+{
+    Py_intptr_t shape[2] = {mDimX, mDimY};
+    np::ndarray result = np::zeros(2, shape, np::dtype::get_builtin<double>());
+    std::copy(&mSpins(0, 0), &mSpins(mDimX-1, mDimY-1)+1, reinterpret_cast<double*>(result.get_data()));
+    return result;
+};
+
+
+void IsingSpin2D::setTemperature(double temperature)
+{
+    mTemperature = temperature;
+};
---
+++ backend/clib/src/simulators/condensed_matter/spin/ising_spin_2D.hpp
@@ -0,0 +1,41 @@
+#include <chrono>
+#include <random>
+#include <boost/python.hpp>
+#include <boost/python/numpy.hpp>
+#include <Eigen/Dense>
+
+
+using namespace Eigen;
+
+namespace p = boost::python;
+namespace np = boost::python::numpy;
+
+std::random_device rd;
+
+class IsingSpin2D
+{
+public:
+    IsingSpin2D(int dimX, int dimY, double initTemperature);
+    ~IsingSpin2D() {};
+    void update(double dt);
+    np::ndarray getSpins();
+    void setTemperature(double temperature);
+
+private:
+    MatrixXd mSpins;
+    int mCnt;
+    int const mDimX;
+    int const mDimY;
+    double mTemperature; 
+};
+
+BOOST_PYTHON_MODULE(ising_spin_2D) {
+    Py_Initialize();
+    np::initialize();
+    p::class_<IsingSpin2D>(
+        "IsingSpin2D", 
+        p::init<int, int, double>())
+        .def("update", &IsingSpin2D::update)
+        .def("getSpins", &IsingSpin2D::getSpins)
+        .def("setTemperature", &IsingSpin2D::setTemperature);
+}
